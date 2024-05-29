356a63fc2266fd333e9155591f027af538475e40
Manosh1ts <166797051+Manosh1ts@users.noreply.github.com>
2024-05-28 23:59:57 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,13 @@
-java -Xmx10G -jar server.jar nogui
+#!/bin/bash
+
+# Especifica o caminho para o diretório de trabalho
+WORKSPACE_DIR="/workspaces/CoisoCraft/op"
+
+# Verifica se o diretório de trabalho existe, se não existir, cria-o
+mkdir -p "$WORKSPACE_DIR"
+
+# Define o diretório de trabalho
+cd "$WORKSPACE_DIR"
+
+# Inicia o servidor PaperMC usando o Java 16 em segundo plano
+java -Xmx10G -jar minecraft_server.1.16.5.jar nogui
